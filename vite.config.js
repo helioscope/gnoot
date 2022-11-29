32c939a37fa9ea16f51b424b08c204ec2274048a
s <11468529+helioscope@users.noreply.github.com>
2022-11-28 21:54:20 -0800

    edit vite config to make asset paths relative, to player nicer with itch (i.e. not have to hand-edit dist/index.html between building & uploading)

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ export default defineConfig({
 	plugins: [],
 	server: { host: '0.0.0.0', port: 4321 },
 	clearScreen: false,
+	base: './' // default "/" makes itch unhappy -- it wants relative links
 })
